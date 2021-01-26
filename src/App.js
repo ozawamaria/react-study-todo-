cddfc9bc1bb19ee4e0fc93d87d8839684fd39d89
ozawamaria <2ty2maria1999@gmail.com>
2021-01-26 20:57:13 +0900

    todo!

--- src/App.js
+++ src/App.js
@@ -1,38 +1,87 @@
-import React, { Component } from 'react';
-import TodoList from './TodoList';/*TodoList.jsで指定したTodoListという関数を使う */
+import React, { Component } from 'react'
+import TodoList from './TodoList'
+import Form from './Form'
 import './css/App.css'
+
 
 class App extends Component {
 
   constructor() {
     super()
+    const todos = []
     this.state = {
-      todos: [
-        {
-          id: 1,
-          title: "Hello, React!",
-          desc: "React始めました",
-          done: false
-        },
-        {
-          id: 2,
-          title: "Hello, Redux!",
-          desc: "Reduxも始めました",
-          done: false
-        },
-      ]
+      isLoading: false,
+      hasError: false,
+      todos: todos,
+      countTodo: todos.length + 1,
     }
   }
 
-  //htmlとして渡したい変数などを設定
+  handleSubmit(e) {
+    e.preventDefault();
+    const title = e.target.title.value;
+    const desc = e.target.desc.value;
+    const todos = this.state.todos.slice()
+    const countTodo = this.state.countTodo
+
+    todos.push({
+      id: countTodo,
+      title: title,
+      desc: desc,
+      done: false,
+    });
+
+    this.setState({ todos })
+    this.setState({ countTodo: countTodo + 1 })
+
+
+    e.target.title.value = '';
+    e.target.desc.value = '';
+  }
+
+  setTodoStatus(clickTodo) {
+    const todos = this.state.todos.slice();
+    const todo = todos[clickTodo.id - 1];
+    todo.done = !todo.done;
+    todos[clickTodo.id - 1] = todo;
+
+    this.setState({ todos });
+  }
+  fetchData(url) {
+    this.setState({ isLoading: true })
+    fetch(url)
+      .then((response) => {
+        console.log(response)
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        this.setState({ isLoading: false })
+        return response
+      })
+      .then((response) => response.json())
+      .then((data) => {
+        let countTodo = this.state.countTodo
+        const todos = data.map(data => {
+          const todo = Object.assign({}, data, { id: countTodo++, done: false })
+          return todo
+        })
+        this.setState({ todos, countTodo })
+      })
+      .catch(() => this.setState({ hasError: true }))
+  }
+  componentDidMount() {
+    this.fetchData('data.json');
+  }
   render() {
-    /*実際に返したいhtmlそのものを記述
-    今回は、TodoList.jsでtodosという変数が使えるようになった*/
     return (
       <div className="app">
-        <h1>todoアプリを作ってみた</h1>
+        <h1>todoアプリ</h1>
+        <Form handleSubmit={this.handleSubmit.bind(this)} />
         <TodoList
           todos={this.state.todos}
+          setTodoStatus={this.setTodoStatus.bind(this)}
+          isLoading={this.state.isLoading}
+          hasError={this.state.hasError}
           />
       </div>
     );
